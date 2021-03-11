c4825c7ecbc4713406b910595e56a2183d4c5a0c
Sparrow007 <ankit.steven007@gmail.com>
2021-03-11 23:35:15 +0530

    adding reflection imageview required methods

--- app/src/main/java/com/example/customviewimple/customview/ReflectionView.kt
+++ app/src/main/java/com/example/customviewimple/customview/ReflectionView.kt
@@ -1,11 +1,23 @@
 package com.example.customviewimple.customview
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 
 class ReflectionView(context: Context, attrs: AttributeSet): AppCompatImageView(context, attrs) {
 
-    
+    override fun setImageDrawable(drawable: Drawable?) {
+        super.setImageDrawable(drawable)
+    }
+
+    override fun setImageResource(resId: Int) {
+        super.setImageResource(resId)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
 
 }
