a2a12ef29f940fff6b65a2cede576ec40988565e
Sparrow007 <ankit.steven007@gmail.com>
2021-03-12 00:02:05 +0530

    fix activity in manifest

--- app/src/main/java/com/example/customviewimple/customview/ReflectionView.kt
+++ app/src/main/java/com/example/customviewimple/customview/ReflectionView.kt
@@ -1,7 +1,11 @@
 package com.example.customviewimple.customview
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.Matrix
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
@@ -13,11 +17,37 @@ class ReflectionView(context: Context, attrs: AttributeSet): AppCompatImageView(
     }
 
     override fun setImageResource(resId: Int) {
-        super.setImageResource(resId)
+        getReflection(BitmapFactory.decodeResource(resources, resId))
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
     }
 
+    private fun getReflection(image: Bitmap) {
+
+        val reflectionGap = 4f
+        val matrix = Matrix()
+        matrix.preScale(1f, -1f)
+
+        val originalImageWidth = image.width
+        val originalImageHeight = image.height
+
+        val reflectionImage = Bitmap.createBitmap(image, 0, originalImageHeight/2, originalImageWidth,
+        originalImageHeight/2, matrix, false)
+
+        val bitmapWithReflection = Bitmap.createBitmap(originalImageWidth, originalImageHeight + originalImageHeight/2,
+        Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(bitmapWithReflection)
+
+        //draw original image
+        canvas.drawBitmap(image, 0f, 0f, null)
+
+        //draw reflection image
+        canvas.drawBitmap(reflectionImage, 0f, originalImageHeight + reflectionGap, null)
+
+        super.setImageDrawable(BitmapDrawable(resources, bitmapWithReflection))
+
+    }
 }
