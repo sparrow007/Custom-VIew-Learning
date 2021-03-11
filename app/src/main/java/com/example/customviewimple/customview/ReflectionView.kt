706877811d264722a92ab0a414906a2506f0ce3a
Sparrow007 <ankit.steven007@gmail.com>
2021-03-11 23:34:07 +0530

    adding reflection view

---
+++ app/src/main/java/com/example/customviewimple/customview/ReflectionView.kt
@@ -0,0 +1,11 @@
+package com.example.customviewimple.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class ReflectionView(context: Context, attrs: AttributeSet): AppCompatImageView(context, attrs) {
+
+    
+
+}
