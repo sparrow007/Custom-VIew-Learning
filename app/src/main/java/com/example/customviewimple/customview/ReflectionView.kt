2acd0e69430bd5a6c05dcdae98c2bdff20e63138
Sparrow007 <ankit.steven007@gmail.com>
2021-03-12 01:27:50 +0530

    refactor method in reflection view

--- app/src/main/java/com/example/customviewimple/customview/ReflectionView.kt
+++ app/src/main/java/com/example/customviewimple/customview/ReflectionView.kt
@@ -14,7 +14,7 @@ class ReflectionView(context: Context, attrs: AttributeSet): AppCompatImageView(
         if (drawable is BitmapDrawable) {
             val bitmapDrawable = drawable as BitmapDrawable
             if (bitmapDrawable.bitmap != null) {
-                doReflection(bitmapDrawable.bitmap)
+                performReflection(bitmapDrawable.bitmap)
             }
         }
 
@@ -32,74 +32,15 @@ class ReflectionView(context: Context, attrs: AttributeSet): AppCompatImageView(
         val canvas = Canvas(bitmap)
         drawable.setBounds(0, 0, canvas.width, canvas.height)
         drawable.draw(canvas)
-        doReflection(bitmap)
+        performReflection(bitmap)
 
     }
 
     override fun setImageResource(resId: Int) {
-        doReflection(BitmapFactory.decodeResource(resources, resId))
+        performReflection(BitmapFactory.decodeResource(resources, resId))
     }
 
-
-    private fun getReflection(image: Bitmap?) {
-
-        if (image == null ) return
-
-        val reflectionGap = 4f
-        val matrix = Matrix()
-        matrix.preScale(1f, -1f)
-
-        val originalImageWidth = image.width
-        val originalImageHeight = image.height
-
-        val reflectionImage = Bitmap.createBitmap(
-            image, 0, originalImageHeight / 2, originalImageWidth,
-            originalImageHeight / 2, matrix, false
-        )
-
-        val bitmapWithReflection = Bitmap.createBitmap(
-            originalImageWidth, originalImageHeight + originalImageHeight / 2,
-            Bitmap.Config.ARGB_8888
-        )
-
-        val canvas = Canvas(bitmapWithReflection)
-
-        //draw original image
-        canvas.drawBitmap(image, 0f, 0f, null)
-
-        //draw reflection image
-        canvas.drawBitmap(reflectionImage, 0f, originalImageHeight + reflectionGap, null)
-
-        val paint = Paint()
-        //Adding shading to the paint for the gradient
-        val shader = LinearGradient(
-            0f,
-            originalImageHeight.toFloat(),
-            0f,
-            bitmapWithReflection.height + reflectionGap,
-            0x70ffffff,
-            0x00000000,
-            Shader.TileMode.CLAMP
-        )
-        paint.shader = shader
-        //Apply porterduff in the rect shape
-        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_IN)
-        paint.alpha = 250
-
-        //draw rect with paint
-        canvas.drawRect(
-            0f,
-            originalImageHeight.toFloat(),
-            originalImageWidth.toFloat(),
-            reflectionGap + bitmapWithReflection.height + 20,
-            paint
-        )
-
-        super.setImageDrawable(BitmapDrawable(resources, bitmapWithReflection))
-
-    }
-
-    private fun doReflection(originalImage: Bitmap?) {
+    private fun performReflection(originalImage: Bitmap?) {
         if (originalImage == null) return
         val reflectionGap = 4
         val width = originalImage.width
