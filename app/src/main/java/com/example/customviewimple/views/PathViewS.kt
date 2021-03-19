05b62074f43d36035f67e504f162758f433acd47
Sparrow007 <ankit.steven007@gmail.com>
2021-03-19 08:49:14 +0530

    merge paths update

--- app/src/main/java/com/example/customviewimple/views/PathViewS.kt
+++ app/src/main/java/com/example/customviewimple/views/PathViewS.kt
@@ -26,16 +26,19 @@ class PathViewS(context: Context, attributeSet: AttributeSet) : View(context,att
 
         //Start from center fo the screen
         canvas.translate(width / 2f, height /2f)
+        canvas.scale(1f, -1f)
 
         //path.lineTo(200f, 200f)
 
         //Move to
 
         path.addRect(-200f, -200f, 200f,
-            200f, Path.Direction.CCW)
+            200f, Path.Direction.CW)
 
-        path.setLastPoint(-300f, 300f)
+        val src = Path()
+        src.addCircle(0f,0f,100f, Path.Direction.CW)
 
+        path.addPath(src, 0f, 200f)
 
         canvas.drawPath(path, paint)
     }
