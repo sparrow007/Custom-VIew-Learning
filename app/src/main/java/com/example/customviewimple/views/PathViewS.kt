245b9a59337bbe3450a4464f860ab70f803f3b54
Sparrow007 <ankit.steven007@gmail.com>
2021-03-19 10:16:39 +0530

    arc path update

--- app/src/main/java/com/example/customviewimple/views/PathViewS.kt
+++ app/src/main/java/com/example/customviewimple/views/PathViewS.kt
@@ -1,10 +1,7 @@
 package com.example.customviewimple.views
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
@@ -28,17 +25,11 @@ class PathViewS(context: Context, attributeSet: AttributeSet) : View(context,att
         canvas.translate(width / 2f, height /2f)
         canvas.scale(1f, -1f)
 
-        //path.lineTo(200f, 200f)
+        path.lineTo(100f, 100f)
 
-        //Move to
+        val rect = RectF(0f,0f, 300f, 300f)
 
-        path.addRect(-200f, -200f, 200f,
-            200f, Path.Direction.CW)
-
-        val src = Path()
-        src.addCircle(0f,0f,100f, Path.Direction.CW)
-
-        path.addPath(src, 0f, 200f)
+        path.addArc(rect, 0f, 270f)
 
         canvas.drawPath(path, paint)
     }
