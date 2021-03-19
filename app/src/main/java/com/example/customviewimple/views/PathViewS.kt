debd64b6600b3a4e5afbffa8044bce46341a8c46
Sparrow007 <ankit.steven007@gmail.com>
2021-03-19 11:46:32 +0530

    spider web path update

--- app/src/main/java/com/example/customviewimple/views/PathViewS.kt
+++ app/src/main/java/com/example/customviewimple/views/PathViewS.kt
@@ -4,11 +4,19 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.annotation.MainThread
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 class PathViewS(context: Context, attributeSet: AttributeSet) : View(context,attributeSet) {
 
     private val paint = Paint()
     private val path = Path()
+
+    private var mCenterX = 0
+    private var mCenterY = 0
+    private var mRadius = 50
     init {
 
         paint.color = Color.RED
@@ -17,20 +25,32 @@ class PathViewS(context: Context, attributeSet: AttributeSet) : View(context,att
 
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCenterX = w / 2
+        mCenterY = h / 2
+
+        path.moveTo(mCenterX.toFloat() + mRadius, mCenterY.toFloat())
+    }
+
+    /**
+     * Draw a simple hexagon
+     */
+
     override fun onDraw(canvas: Canvas?) {
 
         if(canvas == null) return
 
         //Start from center fo the screen
-        canvas.translate(width / 2f, height /2f)
-        canvas.scale(1f, -1f)
+        val angle = 2 * PI / 6
 
-        path.lineTo(100f, 100f)
+        
 
-        val rect = RectF(0f,0f, 300f, 300f)
-
-        path.arcTo(rect, 0f, 270f)
-
+        for (i in 1..6) {
+            val x: Float = (mRadius * cos(angle*i)).toFloat()
+            val y = (mRadius * sin(angle*i)).toFloat()
+            path.lineTo(mCenterX + x, mCenterY+ y)
+        }
         canvas.drawPath(path, paint)
     }
 
