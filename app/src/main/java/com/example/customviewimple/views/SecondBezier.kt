2a88020683db007228c72be41be5e44ef72d766b
Sparrow007 <ankit.steven007@gmail.com>
2021-03-20 08:23:21 +0530

    initial setup for second order

---
+++ app/src/main/java/com/example/customviewimple/views/SecondBezier.kt
@@ -0,0 +1,15 @@
+package com.example.customviewimple.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class SecondBezier(context: Context, attributeSet: AttributeSet): View(context, attributeSet) {
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+
+}
