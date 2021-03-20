46eb649557f477dff38606237a9c5849440315db
Sparrow007 <ankit.steven007@gmail.com>
2021-03-20 10:48:48 +0530

    third order bezier curve points draw

--- app/src/main/java/com/example/customviewimple/views/ThirdBezier.kt
+++ app/src/main/java/com/example/customviewimple/views/ThirdBezier.kt
@@ -1,18 +1,55 @@
 package com.example.customviewimple.views
 
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
 class ThirdBezier (context: Context, attributeSet: AttributeSet): View(context, attributeSet){
 
 
+    private val paint = Paint()
+    private val path = Path()
+    private var start = PointF()
+    private var end = PointF()
+    private var controlF = PointF()
+    private var controlS = PointF()
+
+    private var centerX = 0
+    private var centerY = 0
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+
+        centerX = w / 2
+        centerY = h / 2
+
+        start.x = centerX - 200f
+        start.y = centerY.toFloat()
+
+        end.x = centerX + 200f
+        end.y = centerY.toFloat()
+
+        controlF.x = centerX - 200f
+        controlF.y = centerY - 200f
+
+        controlS.x = centerX + 200f
+        controlS.y = centerY - 200f
+
+        paint.color = Color.BLACK
+        paint.strokeWidth = 10f
+
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+
+        if (canvas == null) return
+        //draw points
+        canvas.drawPoint(start.x, start.y, paint)
+        canvas.drawPoint(end.x, end.y, paint)
+        canvas.drawPoint(controlF.x, controlF.y, paint)
+        canvas.drawPoint(controlS.x, controlS.y, paint)
+
     }
 }
