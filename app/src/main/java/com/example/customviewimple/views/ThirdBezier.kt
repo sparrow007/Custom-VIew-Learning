eede7286594bde959d36b9e1442136b157b8dc10
Sparrow007 <ankit.steven007@gmail.com>
2021-03-20 10:41:55 +0530

    third order bezier curve initialize

---
+++ app/src/main/java/com/example/customviewimple/views/ThirdBezier.kt
@@ -0,0 +1,18 @@
+package com.example.customviewimple.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class ThirdBezier (context: Context, attributeSet: AttributeSet): View(context, attributeSet){
+
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+}
