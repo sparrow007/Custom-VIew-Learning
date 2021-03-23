1faf115d6019e15c9bc09f8c52b3c1ff9f456f98
Sparrow007 <ankit.steven007@gmail.com>
2021-03-23 12:01:08 +0530

    adding attribute for qq

--- app/src/main/java/com/example/customviewimple/views/qqmessage/QQMessage.kt
+++ app/src/main/java/com/example/customviewimple/views/qqmessage/QQMessage.kt
@@ -3,6 +3,7 @@ package com.example.customviewimple.views.qqmessage
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -21,6 +22,28 @@ class QQMessage (context: Context, attributeSet: AttributeSet): View(context, at
     private var mSmallCircleShowRadius = 40f
     private var mSmallCircleHideRadius = 15f
     private var mSmallCircleRadius = mSmallCircleShowRadius
+
+    private var p1x = 0f
+    private var p1y = 0f
+
+    private var p2x = 0f
+    private var p2y = 0f
+
+    private var p3x = 0f
+    private var p3y = 0f
+
+    private var p4x = 0f
+    private var p4y = 0f
+
+    private var mControlx = 0f
+    private var mControly = 0f
+    private var dx = 0f
+    private var dy = 0f
+
+    private var mAngle = 0f
+
+    private var bezierPath = Path()
+    private var mBezierPath = Path()
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
 
