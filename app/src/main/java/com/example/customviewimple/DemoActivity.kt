1bf2a270143cb350f392d90af2de79de9c31c8f0
Sparrow007 <ankit.steven007@gmail.com>
2021-04-03 11:45:07 +0530

    adding more animation

--- app/src/main/java/com/example/customviewimple/DemoActivity.kt
+++ app/src/main/java/com/example/customviewimple/DemoActivity.kt
@@ -10,7 +10,10 @@ class DemoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
 
-        recycler.animateCircle()
+        button.setOnClickListener {
+            recycler.animateCircle()
+        }
+
 
     }
 }
