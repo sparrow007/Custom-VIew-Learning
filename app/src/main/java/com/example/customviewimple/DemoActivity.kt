33dee4d53e94865d211b04f9389bd4be5cdf4104
Sparrow007 <ankit.steven007@gmail.com>
2021-04-03 11:35:56 +0530

    adding animator set

--- app/src/main/java/com/example/customviewimple/DemoActivity.kt
+++ app/src/main/java/com/example/customviewimple/DemoActivity.kt
@@ -3,11 +3,14 @@ package com.example.customviewimple
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jackandphantom.carouselrecyclerview.CarouselRecyclerview
+import kotlinx.android.synthetic.main.activity_demo.*
 
 class DemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
+
+        recycler.animateCircle()
 
     }
 }
--- app/src/main/java/com/example/customviewimple/waterdrop/CircleDropView.kt
+++ app/src/main/java/com/example/customviewimple/waterdrop/CircleDropView.kt
@@ -1,21 +1,31 @@
 package com.example.customviewimple.waterdrop
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import android.view.animation.LinearInterpolator
+import androidx.core.animation.addListener
 
 class CircleDropView (context: Context, attributeSet: AttributeSet): View(context, attributeSet) {
 
-    private val radius = 50f
+    private val radius = 20f
     private var mCenterX = 0f
     private var mCenterY = 0f
     private val paint = Paint().apply {
         color = Color.BLACK
         style = Paint.Style.FILL
     }
+
+    private var lastCenter = 0f
+
+    private lateinit var animator: AnimatorSet
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -25,6 +35,7 @@ class CircleDropView (context: Context, attributeSet: AttributeSet): View(contex
         super.onSizeChanged(w, h, oldw, oldh)
         mCenterX = w / 2f
         mCenterY = h / 2f
+        lastCenter = mCenterY
     }
 
 
@@ -34,6 +45,57 @@ class CircleDropView (context: Context, attributeSet: AttributeSet): View(contex
         if (canvas == null) return
 
         canvas.drawCircle(mCenterX, mCenterY, radius, paint)
+
+       // Log.e("MY TAG", "CENTER Y " + mCenterY)
+    }
+
+     fun animateCircle() {
+
+       val valueAnimator = ValueAnimator.ofFloat(0f, 50f)
+        valueAnimator.interpolator = LinearInterpolator()
+
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            Log.e("MY TAG", "animate value" + value)
+
+            mCenterY = lastCenter - value
+            invalidate()
+        }
+
+         valueAnimator.addListener(object : Animator.AnimatorListener {
+             override fun onAnimationStart(animation: Animator?) {
+
+             }
+
+             override fun onAnimationEnd(animation: Animator?) {
+                 lastCenter = mCenterY
+             }
+
+             override fun onAnimationCancel(animation: Animator?) {
+
+             }
+
+             override fun onAnimationRepeat(animation: Animator?) {
+
+             }
+
+         })
+
+         val downAnimator = ValueAnimator.ofFloat(0f, 50f)
+         downAnimator.interpolator = LinearInterpolator()
+         downAnimator.addUpdateListener {
+             val value = it.animatedValue as Float
+             Log.e("MY TAG", "animate value" + value)
+
+             mCenterY = lastCenter + value
+
+             invalidate()
+         }
+
+        animator = AnimatorSet()
+        animator.play(valueAnimator).before(downAnimator)
+         animator.duration = 500
+        animator.start()
     }
 
 }
