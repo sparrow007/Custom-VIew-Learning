0046f796b9b9fda505913f93158f05a7bdbd50af
Sparrow007 <ankit.steven007@gmail.com>
2021-04-24 15:23:16 +0530

    adding polygon view

---
+++ app/src/main/java/com/example/customviewimple/views/Polygons.kt
@@ -0,0 +1,67 @@
+package com.example.customviewimple.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
+class Polygons(context:Context, attributeSet: AttributeSet): View(context, attributeSet) {
+
+
+    private val paint = Paint().apply {
+        color = Color.BLACK
+        style = Paint.Style.FILL
+    }
+
+    private val path = Path()
+
+    var sides: Int = 3
+    var radius: Int = 60
+    private var angle:Float = 60f
+
+    private var initialX = 0f
+    private var initialY = 0f
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        initialX = w/2f + radius
+        initialY = h / 2f
+        angle = ((2 * PI) / 3f).toFloat()
+
+        path.moveTo(initialX, initialY)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        if (canvas == null) return
+
+        for (i in 1..sides) {
+            val y = radius * sin(angle * i)
+            val x = radius * cos(angle * i)
+
+            path.lineTo(initialX + x, initialY + y)
+        }
+
+        path.close()
+        canvas.drawPath(path, paint)
+
+    }
+
+    fun improveSides(side: Int) {
+        angle = 360f / side
+    }
+
+
+
+}
