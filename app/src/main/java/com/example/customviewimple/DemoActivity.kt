106ae6a42ae621be5cf1f35f590935397cea527e
Sparrow007 <ankit.steven007@gmail.com>
2021-05-09 19:54:03 +0530

    refactoring

--- app/src/main/java/com/example/customviewimple/DemoActivity.kt
+++ app/src/main/java/com/example/customviewimple/DemoActivity.kt
@@ -23,10 +23,8 @@ class DemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
+        window.addFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_demo)
 
     }
