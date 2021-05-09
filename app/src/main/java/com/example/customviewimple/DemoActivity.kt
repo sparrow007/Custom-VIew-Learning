35bd1fcfb037ce45a732eeb99097a05d63856345
Sparrow007 <ankit.steven007@gmail.com>
2021-05-09 08:46:39 +0530

    adding animated pathview

--- app/src/main/java/com/example/customviewimple/DemoActivity.kt
+++ app/src/main/java/com/example/customviewimple/DemoActivity.kt
@@ -1,21 +1,55 @@
 package com.example.customviewimple
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import android.view.Window
 import android.view.WindowManager
-import android.widget.SeekBar
-import com.example.customviewimple.kotlinLearn.LearnClass
-import com.jackandphantom.carouselrecyclerview.CarouselRecyclerview
+import android.view.animation.LinearInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import com.example.customviewimple.views.path.AnimatedPathView
 import kotlinx.android.synthetic.main.activity_demo.*
 
 class DemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
         setContentView(R.layout.activity_demo)
+
+
+        val view: AnimatedPathView = findViewById<View>(R.id.animated_path) as AnimatedPathView
+
+        val observer: ViewTreeObserver = view.getViewTreeObserver()
+        if (observer != null && view is AnimatedPathView) {
+            observer.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+
+                    view.getViewTreeObserver().removeGlobalOnLayoutListener(this)
+
+                    view.setPath(floatArrayOf(0f, 0f),
+                        floatArrayOf(view.getWidth().toFloat(), 0f),
+                        floatArrayOf(view.getWidth().toFloat(), view.getHeight().toFloat()),
+                        floatArrayOf(0f, view.getHeight().toFloat()),
+                        floatArrayOf(0f, 0f),
+                        floatArrayOf(view.getWidth().toFloat(), view.getHeight().toFloat()),
+                        floatArrayOf(view.getWidth().toFloat(), 0f),
+                        floatArrayOf(0f, view.getHeight().toFloat()))
+                }
+            })
+        }
+
+        view.setOnClickListener(View.OnClickListener { view ->
+            val anim = ObjectAnimator.ofFloat(view, "percentage", 0.0f, 1.0f)
+            anim.duration = 2000
+            anim.interpolator = LinearInterpolator()
+            anim.start()
+        })
 
     }
 
