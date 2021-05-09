4408611ab6093115892651ee68654cee663b221d
Sparrow007 <ankit.steven007@gmail.com>
2021-05-09 10:32:42 +0530

    adding rect

--- app/src/main/java/com/example/customviewimple/views/path/ObjectFollow.kt
+++ app/src/main/java/com/example/customviewimple/views/path/ObjectFollow.kt
@@ -17,18 +17,22 @@ class ObjectFollow(context: Context, attributeSet: AttributeSet): View(context, 
 
     private var pathLength = 0f
     private var pathProgress = 0f
-    lateinit var pathEffect: DashPathEffect
+
+
+    private val initialMoveX = 300f
+    private var initialMoveY = 0f
+
+    private val cornerPathEffect = CornerPathEffect(15f)
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
-        path.moveTo(100f, height/2f)
-//       val rect = RectF(0f, 0f, 200f, 200f)
-//        path.addRect(rect,Path.Direction.CW)
-        path.lineTo(450f,height/2f)
-        path.lineTo(450f, height/2f+ 400f)
-        path.lineTo(100f, height/2f+400f)
-        path.lineTo(100f, height/2f)
+        initialMoveY = height / 2f
+        path.moveTo(initialMoveX, initialMoveY)
+        path.lineTo(initialMoveX + 450f ,height/2f)
+        path.lineTo(initialMoveX + 450f, height/2f+ 400f)
+        path.lineTo(initialMoveX, height/2f+400f)
+        path.lineTo(initialMoveX, height/2f)
 
         val pathMeasure = PathMeasure(path, false)
         pathLength = pathMeasure.length
@@ -41,8 +45,6 @@ class ObjectFollow(context: Context, attributeSet: AttributeSet): View(context, 
         if(canvas == null) return
         //canvas.translate(width / 2f, height / 2f)
 
-        pathEffect = DashPathEffect(floatArrayOf(pathLength, pathLength), pathLength - pathLength * pathProgress)
-        paint.pathEffect = pathEffect
         canvas.drawPath(path, paint)
 
     }
@@ -50,6 +52,8 @@ class ObjectFollow(context: Context, attributeSet: AttributeSet): View(context, 
 
     fun setPercentage(percentage: Float) {
         this.pathProgress = percentage
+        val pathEffect = DashPathEffect(floatArrayOf(pathLength, pathLength), pathLength - pathLength * pathProgress)
+        paint.pathEffect = ComposePathEffect(pathEffect, cornerPathEffect)
         invalidate()
     }
 
