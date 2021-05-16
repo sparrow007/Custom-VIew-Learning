fb9c96d51de7d57ef9ad855aba639e62bedf30c1
Sparrow007 <ankit.steven007@gmail.com>
2021-05-16 23:56:05 +0530

    adding myPoint

---
+++ app/src/main/java/com/example/customviewimple/views/cupview/MyPoint.java
@@ -0,0 +1,30 @@
+package com.example.customviewimple.views.cupview;
+
+public class MyPoint {
+    private float x;
+    private float y;
+
+    public MyPoint() {
+    }
+
+    public MyPoint(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+}
