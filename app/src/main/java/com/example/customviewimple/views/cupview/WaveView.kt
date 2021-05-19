8ebe2261a7745ffbfcdb10239186580bf2446e26
Sparrow007 <ankit.steven007@gmail.com>
2021-05-19 07:12:35 +0530

    adding circle

--- app/src/main/java/com/example/customviewimple/views/cupview/WaveView.kt
+++ app/src/main/java/com/example/customviewimple/views/cupview/WaveView.kt
@@ -11,22 +11,25 @@ import android.view.View
 class WaveView (context:Context, attributeSet: AttributeSet): View(context, attributeSet) {
 
     private val paint = Paint().apply {
-        color = Color.BLUE
+        color = Color.YELLOW
     }
 
     private val bezierPath = Path()
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        bezierPath.addCircle(200f, 200f, 50f, Path.Direction.CW)
+        bezierPath.addCircle(200f, 200f, 250f, Path.Direction.CW)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
         if (canvas == null) return
+        canvas.save()
         canvas.clipPath(bezierPath)
         canvas.drawColor(Color.BLUE)
+        canvas.restore()
+        canvas.drawCircle(100f, 100f, 50f, paint)
     }
 
 }
