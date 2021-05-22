056801815329d2b66efde07cd30f9309d62beeb5
Sparrow007 <ankit.steven007@gmail.com>
2021-05-22 19:50:09 +0530

    adding cup view

--- app/src/main/java/com/example/customviewimple/views/cupview/CupView.kt
+++ app/src/main/java/com/example/customviewimple/views/cupview/CupView.kt
@@ -108,12 +108,12 @@ class CupView(context: Context, attributeSet: AttributeSet) : View(context, attr
         }
 
         wavePath.lineTo(right - 150f, bottom.toFloat() - 100)
-        canvas.drawPath(wavePath, wavePaint)
+       // canvas.drawPath(wavePath, wavePaint)
 
-//        canvas.drawPath(strawPath, strawPaint)
-//        canvas.drawPath(topBar1Path, paint)
-//        canvas.drawPath(topBar2Path, paint)
- //       canvas.drawPath(bottlePath, bottlePaint)
+        canvas.drawPath(strawPath, strawPaint)
+        canvas.drawPath(topBar1Path, paint)
+        canvas.drawPath(topBar2Path, paint)
+        canvas.drawPath(bottlePath, bottlePaint)
 
         /**
          * Use this for perform the animation when all the required path
