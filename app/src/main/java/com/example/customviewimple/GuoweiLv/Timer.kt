0c8ff7570bec8e5fd8676eda3b84552c621492e2
Sparrow007 <ankit.steven007@gmail.com>
2022-01-24 00:17:18 +0530

    adding the paint and some comment to understand what is going on

--- app/src/main/java/com/example/customviewimple/GuoweiLv/Timer.kt
+++ app/src/main/java/com/example/customviewimple/GuoweiLv/Timer.kt
@@ -1,4 +1,40 @@
 package com.example.customviewimple.GuoweiLv
 
-class Timer {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class Timer (context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    /**
+     * Measure -> First child will measure it's size so that it can tell the parent about the
+     * how much big it going to be so that parent can easily understand and make that much space
+     *
+     * Layout --> After the measure part is done parent will position all of it's children and then
+     * draw itself and request child to do it same
+     *
+     * paint -> In order to some stuff we the paint object
+     *
+     */
+
+    val paint = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.FILL
+        strokeWidth = 10f
+    }
+
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+
+
 }
